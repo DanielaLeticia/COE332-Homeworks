055cdd595c0c4d677aae5e4473c658140b7d3a59
Ubuntu <ubuntu@danisan-vm.js2local>
2023-04-11 18:51:45 +0000

    committing hw

--- homework08/gene_api.py
+++ homework08/gene_api.py
@@ -7,18 +7,17 @@ app = Flask(__name__)
 
 
 def get_redis_client():
-    return redis.Redis(host='127.0.0.1', port=6379, db=0, decode_responses=True)
-
-rd = get_redis_client # would i still need this; what should the function be
-
-redis_ip = os.environ.get('REDIS_IP')
-if not redis_ip:
-    raise Exception()
-rd=redis.Redis(host=redis_ip, port=6379, db=0)
-
+    redis_ip = os.environ.get('REDIS_IP')
+    if not redis_ip:
+        raise Exception()
+    return redis.Redis(host=redis_ip, port=6379, db=0, decode_responses=True)
+rd = get_redis_client # would i also need this??
 
 def get_second_redis_client():
-    return redis.Redis(host='127.0.0.1', port=6379, db=1, decode_responses=True)
+    redis_ip = os.environ.get('REDIS_IP')
+    if not redis_ip:
+        raise Exception()
+    return redis.Redis(host=redis_ip, port=6379, db=1, decode_responses=True)
 rd_second = get_second_redis_client # do i need this, what does this do?
 
 
@@ -97,20 +96,22 @@ def get_gene_info(gene_id_num):
 @app.route('/image', methods=['GET', 'POST', 'DELETE'])
 def get_image():
     '''
-    ...
+    This function will create a plot for the given data, show the user the plot and also delete the plot
+    based on user method input.
+
+    Returns:
+        success or error messages and for the GET method, the user will get the plot image.
     '''
     if request.method == 'GET':
+        rd.get(image_key) # the argument needs the image key that will be stored in the POST method
         return image
 
     elif request.method == 'POST':
-        response = requests.get(url='https://ftp.ebi.ac.uk/pub/databases/genenames/hgnc/json/hgnc_complete_set.json')
-        for item in response.json()['response']['docs']:
-            key = f'{item["hgnc_id"]}'
-            rd.hset(item.get('hgnc_id', json.dumps(item)))
+
         return 'image has been sucessfully loaded.'
 
     elif request.method == 'DELETE':
-        rd_second.flushdb()
+        rd_second.delete() # should i use rd_second or just rd?
         return f'image has been successfully deleted.'
 
     else:
